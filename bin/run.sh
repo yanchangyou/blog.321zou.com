63b687108cf289b54799bcce257bfdb11c33e848
you <yanchangyou@gmail.com>
2018-10-07 10:03:55 +0800

    去掉图片部分

--- bin/run.sh
+++ bin/run.sh
@@ -26,14 +26,3 @@ echo =============run hexo=============
 cd hexo.321zou.com
 bash run.sh
 cd ..
-
-sleep 10
-
-echo ========cp image to hexo========
-cp -r blog.321zou.com/images hexo.321zou.com/public/
-
-echo ========cp image to jekyll======
-mkdir -p jekyll.321zou.com/images
-cp -r blog.321zou.com/images jekyll.321zou.com/
-
-cd ..
