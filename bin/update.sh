e4a1ed1615fe4b3e33234ab897e272ffa67f615e
yanchangyou <15070440@cnsuning.com>
2018-10-07 10:16:07 +0800

    添加更新操作，避免重启，重启导致10秒不可用

---
+++ bin/update.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+echo goto workspace
+cd blog.321zou.com
+
+echo ========update blog docs===========
+cd blog.321zou.com
+git pull
+cd ..
+
+echo ========update theme===========
+cd hexo-theme-next
+git pull
+cd ..
+
+echo ========init docs _posts===========
+cp -r blog.321zou.com/docs/* jekyll.321zou.com/_posts
+cp -r blog.321zou.com/docs/* hexo.321zou.com/source/_posts
+
+echo ============run jekyll============
+cd jekyll.321zou.com
+git pull
+cd ..
+
+echo =============run hexo=============
+cd hexo.321zou.com
+git pull
+cd ..
